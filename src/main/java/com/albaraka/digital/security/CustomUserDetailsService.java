e83a6042741d919a4a16c57e4e6a584a97f4317b
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-17 17:38:53 +0100

    feat: implement loading of user details and roles from database

--- src/main/java/com/albaraka/digital/security/CustomUserDetailsService.java
+++ src/main/java/com/albaraka/digital/security/CustomUserDetailsService.java
@@ -1,4 +1,34 @@
 package com.albaraka.digital.security;
 
-public class CustomUserDetailsService {
+import com.albaraka.digital.model.User;
+import com.albaraka.digital.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found via email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isActive(),
+                true,
+                true,
+                true,
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())));
+    }
 }
