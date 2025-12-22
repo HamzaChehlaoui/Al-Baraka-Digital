ee7275c9fa06687702e8780d58a58779ae5b9e0e
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-22 11:00:33 +0100

    feat: Implement core backend functionalities including user authentication, account, document, and operation management with JWT security.

--- src/main/java/com/albaraka/digital/dto/response/AuthResponse.java
+++ src/main/java/com/albaraka/digital/dto/response/AuthResponse.java
@@ -1,4 +1,21 @@
 package com.albaraka.digital.dto.response;
 
+import com.albaraka.digital.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+
+    private String accessToken;
+
+    private String email;
+    private String fullName;
+    private Role role;
+    private String accountNumber;
 }
--- src/main/java/com/albaraka/digital/service/AuthService.java
+++ src/main/java/com/albaraka/digital/service/AuthService.java
@@ -1,4 +1,14 @@
 package com.albaraka.digital.service;
 
+import com.albaraka.digital.dto.request.LoginRequest;
+import com.albaraka.digital.dto.request.RegisterRequest;
+import com.albaraka.digital.dto.response.AuthResponse;
+
 public interface AuthService {
+
+    AuthResponse login(LoginRequest request);
+
+    AuthResponse register(RegisterRequest request);
+
+
 }
