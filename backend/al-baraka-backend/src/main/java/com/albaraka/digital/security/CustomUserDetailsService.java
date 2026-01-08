3109997403570b77e7a76375fa3261edbec3a290
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-01-08 17:15:24 +0000

    test

--- backend/al-baraka-backend/src/main/java/com/albaraka/digital/security/CustomUserDetailsService.java
+++ backend/al-baraka-backend/src/main/java/com/albaraka/digital/security/CustomUserDetailsService.java
@@ -32,3 +32,4 @@ public class CustomUserDetailsService implements UserDetailsService {
                 Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())));
     }
 }
+
